d8eff705ed05023dc22f347d8d33ef4735dcb0a1
humphreyotieno1 <humphreyotieno04@gmail.com>
2025-08-11 12:53:14 +0300

    Prepare for Render deployment

--- setup.sh
+++ setup.sh
@@ -1,13 +1,3 @@
-#!/bin/bash
-
-# Install dependencies
-pip install --upgrade pip
+#!/usr/bin/env bash
 pip install -r requirements.txt
-
-# Run Django management commands
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic --noinput --clear
-
-# Install drf-yasg static files specifically
-python -c "import os; from drf_yasg import openapi; print('Drf-yasg static files location:', os.path.dirname(openapi.__file__))"
+python manage.py collectstatic --noinput
